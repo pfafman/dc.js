4cabb0e14e2c38485151ce6c35cf2fffc85bd77a
Timothy Pfafman <tim@pfafman.com>
2016-08-31 08:53:33 -0700

    remove debugging

--- src/data-count.js
+++ src/data-count.js
@@ -91,8 +91,6 @@ dc.dataCount = function (parent, chartGroup) {
         var all = _formatNumber(tot);
         var selected = _formatNumber(val);
 
-        console.log(">>> data-count doRender <<<<");
-        
         if ((tot === val) && (_html.all !== '')) {
             _chart.root().html(_html.all.replace('%total-count', all).replace('%filter-count', selected));
         } else if (_html.some !== '') {
